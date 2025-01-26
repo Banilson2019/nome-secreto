c20fb065d6cf1d0e7ab5ed425e99721226347248
Banilson Zua <banilsonzua98@gmail.com>
2025-01-26 20:04:14 +0000

    actualizando a função addAmigo

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let amigos = [];
 
+//função para limpar o campo
 function limparCampo() {
     nome = document.querySelector('input');
     nome.value = '';
 }
-
+//função para adicionar os nome dos amigos
 function adicionarAmigo(){
 
     let nome = document.querySelector('input').value;
-    console.log(nome.length);
     if(nome == ""){
         alert("Por favor digita um nome valido")
     }else{
@@ -24,7 +24,7 @@ function adicionarAmigo(){
 
     }
 }
-
+//função para sotear um nome 
 function sortearAmigo(){
     if(amigos == ""){
         alert("Não podes fazer o sorteio sem digitar os nomes dos seus amigos!!")
